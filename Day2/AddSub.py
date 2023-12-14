1b121294e56355d287620338cf937405ea4d85f3
Pradynesh <pradyneshwagh@gmail.com>
2023-12-14 13:40:01 +0530

    Added Day 2 files

---
+++ Day2/AddSub.py
@@ -0,0 +1,10 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
---
+++ Day2/AreaOfRectangle.py
@@ -0,0 +1,6 @@
+l = float(input('Enter Length of the Rectangle: '))
+b = float(input('Enter Breadth of the Rectangle: '))
+
+area = l * b
+
+print(f"Area of rectangle is {area}")
